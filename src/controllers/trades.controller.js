7bd25e36efde6c02b80552ae3179500cb757011a
hussamhejaz <ooooo.ooooo.hosam@gmail.com>
2026-03-02 18:10:52 +0300

    first commit

--- src/controllers/trades.controller.js
+++ src/controllers/trades.controller.js
@@ -320,7 +320,8 @@ async function createTrade(req, res, next) {
     res.status(201).json({ id: tradeId, ...payload });
 
     const creationText =
-      `🌟 ليست توصية للشراء او البيع 🌟\n\n` +
+      `✨ مقترح جديد 🚀\n` +
+       `🌟 ليست توصية للشراء او البيع 🌟\n`
       `🏢 الرمز: ${payload.symbol}\n` +
       `🏷️ النوع: ${String(payload.right).toUpperCase()}\n` +
       `🎯 السترايك: ${payload.strike}\n` +
